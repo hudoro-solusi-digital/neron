ba77a9ce2159514832052bcd3cc09dfd565f9874
Mikhael Esa <56322741+Mikeultron@users.noreply.github.com>
2021-07-21 09:33:22 +0700

    [#17] v0.1.0 Candidate - Add Avatar component (#18)
    * Fix conflict
    
    * Add default image and onError

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,7 +1,8 @@
+import Avatar from "./Avatar";
 import Button from "./Button";
 import Checkbox from "./Checkbox";
 import Radio from "./Radio";
 import Text from "./Text";
 import Toggler from "./Toggler";
 
-export { Button, Checkbox, Radio, Text, Toggler };
+export { Button, Checkbox, Radio, Text, Toggler, Avatar };
