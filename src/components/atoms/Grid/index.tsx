ed224efc4bd0e4e1ccace05a908bceea9bb7c21d
Mikhael Esa <56322741+Mikeultron@users.noreply.github.com>
2021-07-22 08:35:10 +0700

    [#10] v0.1.0 Candidate - Add Layout component (#19)

---
+++ src/components/atoms/Grid/index.tsx
@@ -0,0 +1,44 @@
+// import React from "react";
+import styled, { css } from "styled-components";
+import { GridSizeType, IGridProps } from "../../../utils";
+
+const totalGrid = 12;
+const multiplier = 100;
+
+const breakpoints = {
+  medium: 992,
+  small: 576,
+};
+
+const calculateFlexBasis = (size: GridSizeType) => {
+  return (size / totalGrid) * multiplier;
+};
+
+const Grid = styled.div<IGridProps>`
+  ${(props) =>
+    props.container &&
+    css`
+      display: flex;
+      flex-wrap: wrap;
+      align-items: ${props.alignItems || "initial"};
+      justify-content: ${props.justifyContent || "initial"};
+      flex-direction: ${props.flexDirection || "initial"};
+    `}
+  ${(props) =>
+    !props.container &&
+    css`
+      box-sizing: border-box;
+      flex-basis: ${props.size ? calculateFlexBasis(props.size) : 0}%;
+      flex-grow: ${props.size ? 0 : 1};
+      @media (max-width: ${breakpoints.medium}px) {
+        flex-basis: ${props.mdSize ? calculateFlexBasis(props.mdSize) : 0}%;
+        flex-grow: ${props.mdSize ? 0 : 1};
+      }
+      @media (max-width: ${breakpoints.small}px) {
+        flex-basis: ${props.smSize ? calculateFlexBasis(props.smSize) : 0}%;
+        flex-grow: ${props.smSize ? 0 : 1};
+      }
+    `};
+`;
+
+export default Grid;
