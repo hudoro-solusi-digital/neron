56f069da0d99f03dca6ff2d791745fa23e10ba8d
Mikeultron <mikeul2.gt@gmail.com>
2021-07-30 01:22:38 +0700

    Fix ?? to ||

--- src/components/atoms/Button/styles.ts
+++ src/components/atoms/Button/styles.ts
@@ -7,7 +7,7 @@ import {
 } from "../../../utils/styles";
 
 export const PrimaryButton = styled.button`
-  background-color: ${(props) => props.theme.background ?? colors.primary[100]};
+  background-color: ${(props) => props.theme.background || colors.primary[100]};
   color: white;
   border-radius: 999px;
   font-family: ${fontFamilies.poppins};
@@ -18,14 +18,14 @@ export const PrimaryButton = styled.button`
 `;
 
 export const SecondaryButton = styled(PrimaryButton)`
-  border: 1px solid ${(props) => props.theme.background ?? colors.primary[100]};
-  color: ${(props) => props.theme.foreground ?? colors.primary[100]};
+  border: 1px solid ${(props) => props.theme.background || colors.primary[100]};
+  color: ${(props) => props.theme.foreground || colors.primary[100]};
   background-color: transparent;
 `;
 
 export const GhostButton = styled(SecondaryButton)`
   border: none;
-  color: ${(props) => props.theme.foreground ?? colors.primary[100]};
+  color: ${(props) => props.theme.foreground || colors.primary[100]};
 `;
 
 PrimaryButton.defaultProps = {
