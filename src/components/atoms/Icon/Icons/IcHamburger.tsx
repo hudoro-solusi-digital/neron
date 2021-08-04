3912925383f4d953edcdef5446c8246bbbb70edf
Mikeultron <mikeul2.gt@gmail.com>
2021-08-04 11:43:57 +0700

    Add icons

---
+++ src/components/atoms/Icon/Icons/IcHamburger.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ISVGProps } from "../../../../utils";
+
+function IcHamburger({ ...rest }: ISVGProps) {
+  return (
+    <svg {...rest} viewBox="64 64 896 896">
+      <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
+    </svg>
+  );
+}
+
+export default IcHamburger;
--- src/components/atoms/Icon/Icons/index.tsx
+++ src/components/atoms/Icon/Icons/index.tsx
@@ -2,11 +2,35 @@ import React from "react";
 import IcEye from "./IcEye";
 import IcEyeSlash from "./IcEyeSlash";
 import IcCheck from "./IcCheck";
+import IcHamburger from "./IcHamburger";
+import IcNotification from "./IcNotification";
+import IcSettings from "./IcSettings";
+import IcUser from "./IcUser";
+import IcLogout from "./IcLogout";
+import IcDashboard from "./IcDashboard";
+import IcSwap from "./IcSwap";
+import IcLogin from "./IcLogin";
+import IcInfo from "./IcInfo";
+import IcHistory from "./IcHistory";
+import IcTrash from "./IcTrash";
+import IcSearch from "./IcSearch";
 
 const Icons = {
   IcEye: <IcEye />,
   IcEyeSlash: <IcEyeSlash />,
   IcCheck: <IcCheck />,
+  IcHamburger: <IcHamburger />,
+  IcNotification: <IcNotification />,
+  IcSettings: <IcSettings />,
+  IcUser: <IcUser />,
+  IcLogout: <IcLogout />,
+  IcDashboard: <IcDashboard />,
+  IcSwap: <IcSwap />,
+  IcLogin: <IcLogin />,
+  IcInfo: <IcInfo />,
+  IcHistory: <IcHistory />,
+  IcTrash: <IcTrash />,
+  IcSearch: <IcSearch />,
 };
 
 export default Icons;
