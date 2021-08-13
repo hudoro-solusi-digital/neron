43c1a174a95e23fd74677fd498b0c46c49175248
Mikhael Esa <56322741+Mikeultron@users.noreply.github.com>
2021-08-14 01:22:04 +0700

    [#39]- Improved LabelValue component (#40)

--- src/components/atoms/LabelValue/styles.ts
+++ src/components/atoms/LabelValue/styles.ts
@@ -2,10 +2,17 @@ import styled from "styled-components";
 import { colors, fontFamilies } from "../../../utils";
 import Text from "../Text";
 
+interface IProps {
+  valueColor?: string;
+}
+
 export const Label = styled(Text)`
   font-family: ${fontFamilies.poppins};
   font-weight: 500;
   color: ${colors.black[60]};
 `;
 
-export const Value = styled(Text)``;
+export const Value = styled(Text)`
+  color: ${(props: IProps) =>
+    props.valueColor ? props.valueColor : colors.black[100]};
+`;
