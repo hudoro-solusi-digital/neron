e293445fdbb0c263963e764c11f3bb340e437690
Mikeultron <mikeul2.gt@gmail.com>
2021-10-25 21:10:49 +0700

    Fix Avatar default image

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -1,14 +1,17 @@
 import React, { SyntheticEvent } from "react";
 import { IAvatarProps } from "../../../utils";
 import { Image } from "./styles";
-import User from "./user.png";
 
 function Avatar({ size, src, ...rest }: IAvatarProps) {
+  const defaultImage =
+    "https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?b=1&k=20&m=1300845620&s=170667a&w=0&h=JbOeyFgAc6-3jmptv6mzXpGcAd_8xqkQa_oUK2viFr8=";
   const onError = (e: SyntheticEvent<HTMLImageElement>) => {
     e.currentTarget.onerror = null;
-    e.currentTarget.src = User;
+    e.currentTarget.src = defaultImage;
   };
-  return <Image {...rest} onError={onError} src={src || User} size={size} />;
+  return (
+    <Image {...rest} onError={onError} src={src || defaultImage} size={size} />
+  );
 }
 
 export default Avatar;
