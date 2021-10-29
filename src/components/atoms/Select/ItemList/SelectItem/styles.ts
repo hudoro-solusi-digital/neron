c46140477019c11b14d197f161f5aad69a1bc565
Mikeultron <mikeul2.gt@gmail.com>
2021-10-29 13:01:46 +0700

    Fixing bad import

--- src/components/atoms/Select/ItemList/SelectItem/styles.ts
+++ src/components/atoms/Select/ItemList/SelectItem/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { colors, fontFamilies } from "../../../../..";
+import { fontFamilies, colors } from "../../../../../utils";
 
 interface IProps {
   selected?: boolean;
